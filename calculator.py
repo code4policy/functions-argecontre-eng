3ad1f3fc117b7395f1b36953db2857264907d24d
Argenis Contreras <your-email@example.com>
2026-01-17 21:23:52 -0500

    calculator with 4 functions

--- calculator.py
+++ calculator.py
@@ -14,9 +14,6 @@ def divide(a, b):
 	return a / b
 
 
-def square(a):
-	return a * a
-
-
-def cube(a):
-	return a * a * a
+print("I'm going use the calculator functions to multiply 5 and 6")
+x = multiply(5, 6)
+print(x)
